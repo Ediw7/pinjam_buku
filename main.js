f1a3694a40955010fcf2bb56c64f8f6d1bf6e7ef
unknown <ediwicoro26@gmail.com>
2023-06-10 14:50:34 +0700

    website untuk pinjam buku

---
+++ main.js
@@ -0,0 +1,15 @@
+const sr = ScrollReveal({
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  reset: true,
+});
+
+sr.reveal(".text", { delay: 200, origin: "top" });
+sr.reveal(".form-container form", { delay: 200, origin: "left" });
+sr.reveal(".heading", { delay: 200, origin: "top" });
+sr.reveal(".read-container .box", { delay: 200, origin: "top" });
+sr.reveal(".services-container .box", { delay: 200, origin: "top" });
+sr.reveal(".about-container .box", { delay: 200, origin: "top" });
+sr.reveal(".reviews-container .box", { delay: 200, origin: "top" });
+sr.reveal(".newsletter .box", { delay: 200, origin: "bottom" });
